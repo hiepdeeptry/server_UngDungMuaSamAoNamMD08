74d34b8b8c10175873c98566052080a4b94ac18a
hiepdeeptry <133551147+hiepdeeptry@users.noreply.github.com>
2024-10-13 00:11:33 +0700

    update

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = require('firebase/auth');
 const { auth } = require('../firebaseConfig');
-
+const notifier = require('node-notifier');
+const path = require('path');
 
 
 exports.auth = (req, res) => {
@@ -17,21 +18,42 @@ exports.auth = (req, res) => {
         signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Đăng nhập thành công
-        
+        notifier.notify({
+            title: 'Đăng nhập thành công',
+            message: 'Chào mừng bạn quay trở lại!',
+            sound: true, // Tùy chọn để phát âm thanh thông báo,
+            appID:"Shop bán áo MD08",
+            icon: path.join(__dirname, '../public/images/logoMD08_128x128.png'),
+          });
         res.redirect('/');
       })
       .catch((error) => {
-        res.status(400).send(error.message);
+        notifier.notify({
+            title: 'Lỗi đăng nhập',
+            message: 'Sai tài khoản hoặc mật khẩu\nĐăng nhập thất bại! Vui lòng thử lại.',
+            sound: true // Tùy chọn để phát âm thanh thông báo
+          });
+        return res.render('login/index');
       });
     }else if(type === 'register'){
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // Đăng ký thành công
-          res.redirect('/users')
+          notifier.notify({
+            title: 'Thành công',
+            message: 'Tạo tài khoản thành công!\n Chào mừng bạn.',
+            sound: true // Tùy chọn để phát âm thanh thông báo
+          });
+          res.redirect('/auth')
         })
         .catch((error) => {
           // Xử lý lỗi
-          res.status(400).send(error.message);
+          notifier.notify({
+            title: 'Lỗi đăng ký',
+            message: 'Sai tài khoản hoặc mật khẩu\nĐăng nhập thất bại! Vui lòng thử lại.',
+            sound: true // Tùy chọn để phát âm thanh thông báo
+          });
+        return  res.status(400).send(error.message);
         });
     }else{
         res.status(400).send('Tham số type không hợp lệ');
@@ -39,12 +61,11 @@ exports.auth = (req, res) => {
     
     
 
-     
-
+    
   };
 
   exports.showForm = (req, res, next) => {
-    res.render('login/index' )
+    res.render('login/index')
 }
 
 //   exports.register = (req, res) => {
